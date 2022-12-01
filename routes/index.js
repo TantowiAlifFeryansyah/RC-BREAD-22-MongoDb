3eec7f6d47fa98619e76210764cfc56262a823ed
Tantowi Alif Feryansyah <tantowialif15@gmail.com>
2022-12-01 15:58:44 +0700

    c22 mongodb sorting

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,19 @@ module.exports = function (db) {
   const User = db.collection('siswa');
   router.get('/', async function (req, res, next) {
     try {
+      // SORT 
+      const fiturSort = {}
+      const sortBY = req.query.sortBy || '_id'
+      const sortMode = req.query.sortMode || 'asc'
+      fiturSort[sortBY] = sortMode == 'asc' ? 1 : -1
+
       // PAGINATIONS
+      const sorting = {}
+      const url = req.url == '/' ? '/?page=1&sortBy=_id&sortMode=asc' : req.url
       const page = req.query.page || 1
       const limitList = 3
       const offset = (page - 1) * limitList
+      sorting[sortBY] = sortMode == 'asc' ? 1 : -1
 
       // SEARCHING
       const wheres = {}
@@ -30,26 +39,32 @@ module.exports = function (db) {
       }
 
       if (req.query.datecheck) {
-        if (req.query.startdate != "" && req.query.enddate != "") {
-          wheres["date"] = {
-            $gte: new Date(`${req.query.startdate}`), $lte: new Date(`${req.query.enddate}`)
+        if (req.query.startdate != '' && req.query.enddate != '') {
+          wheres['date'] = {
+            $gte: new Date(`${req.query.startdate}`), 
+            $lte: new Date(`${req.query.enddate}`)
           }
-        } else if (req.query.startdate != ''){
-          wheres["date"] = { $gte: new Date(`${req.query.startdate}`) };
-        } else if (req.query.enddate != ''){
-          wheres["date"] = { $lte: new Date(`${req.query.enddate}`) };
+        } else if (req.query.startdate != '') {
+          wheres['date'] = { $gte: new Date(`${req.query.startdate}`) };
+        } else if (req.query.enddate != '') {
+          wheres['date'] = { $lte: new Date(`${req.query.enddate}`) };
         }
+        console.log(wheres);
       }
 
       if (req.query.boolean && req.query.booleancheck) {
         wheres['boolean'] = JSON.parse(`${req.query.boolean}`)
       }
 
-      const users = await User.find(wheres).skip(offset).limit(limitList).toArray()
-      const total = await User.find().count(users)
+      const result = await User.find(wheres).toArray()
+      var total = result.length
       const pages = Math.ceil(total / limitList)
 
-      res.render('list', { data: users, moment, page, pages, offset , query: req.query})
+      const users = await User.find(wheres).skip(offset).limit(limitList).sort(sorting).toArray()
+      // const total = await User.find().count(users)
+      // const pages = Math.ceil(total / limitList)
+
+      res.render('list', { data: users, moment, page, pages, offset, query: req.query, url })
     } catch (err) {
       console.log(`list error`, err);
     }
@@ -57,7 +72,7 @@ module.exports = function (db) {
 
   // ROUTES ADD
   router.get('/add', async function (req, res, next) {
-    try {
+    try { 
       res.render('add')
     } catch (err) {
       console.log(`add error`, err);
